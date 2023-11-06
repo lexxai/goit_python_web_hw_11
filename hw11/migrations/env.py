b1a6c59cc96a04029a79d4326a5c3ae542f0927a
lexxai <lexxai@users.noreply.github.com>
2023-11-06 19:03:30 +0200

    db, models

--- hw11/migrations/env.py
+++ hw11/migrations/env.py
@@ -3,6 +3,9 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from src.database import models
+from src.database import db
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -18,12 +21,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+config.set_main_option("sqlalchemy.url", db.SQLALCHEMY_DATABASE_URL)
 
 
 def run_migrations_offline() -> None:

c3934340eac697106437d89823a9e75f155b480e
lexxai <lexxai@users.noreply.github.com>
2023-11-06 21:06:08 +0200

    index-template

---
+++ hw11/main.py
@@ -0,0 +1,44 @@
+import time
+from fastapi import FastAPI, Path, Query, Depends, HTTPException, Request, status
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+
+from src.database.db import get_db
+from src.routes import contacts
+
+
+app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "tilte": "HW11 APP"}) 
+
+
+@app.get("/api/healthchecker")
+def healthchecker(db: Session = Depends(get_db)):
+    try:
+        # Make request
+        result = db.execute(text("SELECT 1")).fetchone()
+        if result is None:
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly"
+            )
+        return {"message": "Welcome to FastAPI on Howe Work 11!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error connecting to the database",
+        )
